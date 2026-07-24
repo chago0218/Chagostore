9c4fec302a7ed4e678c7d1715eec7bcf0163d557
chago0218 <usagefortiktok0218@gmail.com>
2026-07-25 02:16:06 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,93 +1,115 @@
-const adminProducts = document.querySelector("#adminProducts");
-
-let editId = null;
-
+// 관리자 이메일
+const adminEmail = "관리자이메일@gmail.com";
 
 
-// 상품 추가 / 수정
+// 관리자 인증 확인
+
+auth.onAuthStateChanged((user)=>{
+
+
+    if(!user){
+
+        alert("로그인이 필요합니다");
+
+        location.href = "login.html";
+
+        return;
+
+    }
+
+
+
+    if(user.email !== adminEmail){
+
+        alert("관리자 권한이 없습니다");
+
+        location.href = "index.html";
+
+        return;
+
+    }
+
+
+
+});
+
+
+
+
+
+// 상품 추가
 
 function addProduct(){
 
 
-const data = {
+    const name =
+    document.querySelector("#name").value;
 
 
-image:
-document.querySelector("#image").value,
+    const price =
+    document.querySelector("#price").value;
 
 
-name:
-document.querySelector("#name").value,
+    const image =
+    document.querySelector("#image").value;
 
 
-price:
-document.querySelector("#price").value,
+    const description =
+    document.querySelector("#description").value;
 
 
-brand:
-document.querySelector("#brand").value,
+    const size =
+    document.querySelector("#size").value;
 
 
-description:
-document.querySelector("#description").value,
-
-
-size:
-document.querySelector("#size").value,
-
-
-link:
-document.querySelector("#link").value
+    const link =
+    document.querySelector("#link").value;
 
 
 
-};
+    db.collection("products")
+    .add({
 
 
+        name:name,
+
+        price:Number(price),
+
+        image:image,
+
+        description:description,
+
+        size:size,
+
+        link:link,
+
+        createdAt:
+        new Date()
 
 
+    })
 
-if(editId){
-
-
-db.collection("products")
-.doc(editId)
-.update(data)
-.then(()=>{
+    .then(()=>{
 
 
-alert("수정 완료");
+        alert("상품 등록 완료");
 
 
-editId=null;
+        location.reload();
 
 
-location.reload();
+    })
+
+    .catch((error)=>{
 
 
-});
+        console.log(error);
 
 
-}
-
-else{
+        alert("등록 실패");
 
 
-db.collection("products")
-.add(data)
-.then(()=>{
-
-
-alert("등록 완료");
-
-
-location.reload();
-
-
-});
-
-
-}
+    });
 
 
 
@@ -98,160 +120,116 @@ location.reload();
 
 
 
-
-// 상품 불러오기
-
-
-db.collection("products")
-.get()
-.then((snapshot)=>{
-
-
-adminProducts.innerHTML="";
-
-
-
-snapshot.forEach((doc)=>{
-
-
-let data=doc.data();
-
-
-
-adminProducts.innerHTML += `
-
-
-
-<div class="adminCard">
-
-
-<img src="${data.image}">
-
-
-<h3>${data.name}</h3>
-
-
-<p>${data.price}원</p>
-
-
-<p>${data.brand || ""}</p>
-
-
-
-<button onclick="editProduct('${doc.id}')">
-
-수정
-
-</button>
-
-
-
-<button onclick="deleteProduct('${doc.id}')">
-
-삭제
-
-</button>
-
-
-
-</div>
-
-
-`;
-
-
-
-});
-
-
-
-});
-
-
-
-
-
-
-
-// 수정 불러오기
-
-
-function editProduct(id){
-
-
-db.collection("products")
-.doc(id)
-.get()
-.then((doc)=>{
-
-
-let data=doc.data();
-
-
-
-document.querySelector("#image").value=data.image || "";
-
-document.querySelector("#name").value=data.name || "";
-
-document.querySelector("#price").value=data.price || "";
-
-document.querySelector("#brand").value=data.brand || "";
-
-document.querySelector("#description").value=data.description || "";
-
-document.querySelector("#size").value=data.size || "";
-
-document.querySelector("#link").value=data.link || "";
-
-
-
-editId=id;
-
-
-
-window.scrollTo(0,0);
-
-
-
-});
-
-
-}
-
-
-
-
-
-
-
-
-// 삭제
-
+// 상품 삭제
 
 function deleteProduct(id){
 
 
-if(confirm("삭제할까요?")){
+    db.collection("products")
+    .doc(id)
+    .delete()
+
+    .then(()=>{
 
 
-db.collection("products")
-.doc(id)
-.delete()
-.then(()=>{
+        alert("삭제 완료");
 
 
-alert("삭제 완료");
+        location.reload();
 
 
-location.reload();
+    });
 
 
 
-});
+}
+
+
+
+
+
+
+// 관리자 상품 목록 출력
+
+function loadProducts(){
+
+
+    const list =
+    document.querySelector("#adminProducts");
+
+
+
+    if(!list) return;
+
+
+
+    list.innerHTML = "";
+
+
+
+    db.collection("products")
+    .get()
+
+    .then((snapshot)=>{
+
+
+        snapshot.forEach((doc)=>{
+
+
+            let data = doc.data();
+
+
+
+            list.innerHTML += `
+
+
+            <div class="admin-card">
+
+
+            <img src="${data.image}">
+
+
+            <h3>
+
+            ${data.name}
+
+            </h3>
+
+
+            <p>
+
+            ${data.price}원
+
+            </p>
+
+
+
+            <button onclick="deleteProduct('${doc.id}')">
+
+            삭제
+
+            </button>
+
+
+            </div>
+
+
+            `;
+
+
+
+        });
+
+
+
+    });
+
 
 
 }
 
 
 
-}
+
+
+loadProducts();
