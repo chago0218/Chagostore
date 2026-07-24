10a6e55faae021c51b14fb90b484498a5752e03d
chago0218 <usagefortiktok0218@gmail.com>
2026-07-24 23:55:07 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
 
+// Firebase 연결 설정
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcao0Ojq1BnGIBxaUhgUlJZmR6t0Ym-E0",
+  authDomain: "chagostore-9a89c.firebaseapp.com",
+  projectId: "chagostore-9a89c",
+  storageBucket: "chagostore-9a89c.firebasestorage.app",
+  messagingSenderId: "703353726861",
+  appId: "1:703353726861:web:8a6ddac04a65fac7227fc4"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const storage = firebase.storage();
