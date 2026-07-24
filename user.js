ab89adbeb71caffae301625d79c6bf576506df3b
chago0218 <usagefortiktok0218@gmail.com>
2026-07-25 02:44:07 +0900

    Update user.js

--- user.js
+++ user.js
@@ -1,71 +1,38 @@
-const userArea = document.querySelector("#userArea");
-
+const userArea = document.getElementById("userArea");
 
 
 auth.onAuthStateChanged((user)=>{
 
-
     if(user){
 
-
         userArea.innerHTML = `
-
-        <span>
-        ${user.email}
-        </span>
-
-
-        <button onclick="logout()">
-
-        로그아웃
-
-        </button>
-
+            <span>${user.email}</span>
+            <button onclick="logout()">
+                로그아웃
+            </button>
         `;
 
-
-    }
-
-    else{
-
+    }else{
 
         userArea.innerHTML = `
-
-
-        <button onclick="location.href='login.html'">
-
-        로그인
-
-        </button>
-
-
+            <button onclick="location.href='login.html'">
+                로그인
+            </button>
         `;
 
-
     }
-
-
 
 });
 
 
 
-
-
 function logout(){
 
-
     auth.signOut()
-
     .then(()=>{
-
-
-        alert("로그아웃 완료");
 
         location.reload();
 
-
     });
-
 
 }
