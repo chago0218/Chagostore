d74de88fd58ca7320aec9eddd0ccab8861d6e7e6
chago0218 <usagefortiktok0218@gmail.com>
2026-07-25 00:09:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-db.collection("products").onSnapshot((snapshot)=>{
+const products = document.querySelector(".products");
+
+db.collection("products").get().then((snapshot)=>{
 
     snapshot.forEach((doc)=>{
 
-        const product = doc.data();
+        const data = doc.data();
 
-        console.log(product);
+        products.innerHTML += `
+        <div class="card">
+
+            <img src="${data.image || 'https://via.placeholder.com/300'}">
+
+            <div class="info">
+
+                <h3>${data.name}</h3>
+
+                <div class="price">
+                    ${data.price}원
+                </div>
+
+                <p>
+                    ${data.description}
+                </p>
+
+                <p>
+                    사이즈: ${data.size}
+                </p>
+
+                <button class="buy">
+                    구매하기
+                </button>
+
+            </div>
+
+        </div>
+        `;
 
     });
 
