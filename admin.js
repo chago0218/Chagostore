e8415a3cef997b303ecc1a35d5d01468584f388f
chago0218 <usagefortiktok0218@gmail.com>
2026-07-25 01:39:06 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,25 +1,112 @@
-const button = document.querySelector("button");
+const adminProducts = document.querySelector("#adminProducts");
 
-button.addEventListener("click", function(){
 
-    const name = document.querySelectorAll("input")[1].value;
-    const price = Number(document.querySelectorAll("input")[2].value);
-    const description = document.querySelector("textarea").value;
-    const image = document.querySelectorAll("input")[0].value;
-    const size = document.querySelectorAll("input")[3].value;
+// 상품 추가
 
-    db.collection("products").add({
-        image: image,
-        name: name,
-        price: price,
-        description: description,
-        size: size
-    })
-    .then(()=>{
-        alert("상품 등록 완료");
-    })
-    .catch((error)=>{
-        alert("오류: " + error);
-    });
+function addProduct(){
+
+
+const product = {
+
+image: document.querySelector("#image").value,
+
+name: document.querySelector("#name").value,
+
+price: document.querySelector("#price").value,
+
+brand: document.querySelector("#brand").value,
+
+description: document.querySelector("#description").value,
+
+size: document.querySelector("#size").value,
+
+link: document.querySelector("#link").value
+
+
+};
+
+
+
+db.collection("products")
+.add(product)
+.then(()=>{
+
+alert("상품 등록 완료");
+
+location.reload();
 
 });
+
+
+}
+
+
+
+
+// 상품 목록
+
+db.collection("products")
+.get()
+.then((snapshot)=>{
+
+
+snapshot.forEach((doc)=>{
+
+
+const data = doc.data();
+
+
+adminProducts.innerHTML += `
+
+
+<div class="adminCard">
+
+
+<img src="${data.image}">
+
+
+<h3>${data.name}</h3>
+
+
+<p>${data.price}원</p>
+
+
+<button onclick="deleteProduct('${doc.id}')">
+삭제
+</button>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+});
+
+
+
+
+// 삭제
+
+function deleteProduct(id){
+
+
+db.collection("products")
+.doc(id)
+.delete()
+.then(()=>{
+
+
+alert("삭제 완료");
+
+location.reload();
+
+
+});
+
+
+}
