1e367912f0c462031326c7ac05537a31749d9109
chago0218 <usagefortiktok0218@gmail.com>
2026-07-25 01:16:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const products = document.querySelector(".products");
+const pagination = document.querySelector(".pagination");
 
+let allProducts = [];
+let currentPage = 1;
+
+const productsPerPage = 12;
+
+
+// 상품 가져오기
 db.collection("products").get()
 .then((snapshot)=>{
 
     snapshot.forEach((doc)=>{
 
-        const data = doc.data();
+        allProducts.push(doc.data());
+
+    });
+
+    showProducts();
+    createPagination();
+
+})
+.catch((error)=>{
+
+    console.log(error);
+
+});
+
+
+// 상품 표시
+function showProducts(){
+
+    products.innerHTML = "";
+
+
+    let start = (currentPage - 1) * productsPerPage;
+    let end = start + productsPerPage;
+
+
+    let pageProducts = allProducts.slice(start, end);
+
+
+    pageProducts.forEach((data)=>{
 
         products.innerHTML += `
 
@@ -41,9 +77,38 @@ db.collection("products").get()
 
     });
 
-})
-.catch((error)=>{
+}
 
-    console.log(error);
 
-});
+// 페이지 버튼 생성
+function createPagination(){
+
+    pagination.innerHTML = "";
+
+
+    let pageCount = Math.ceil(allProducts.length / productsPerPage);
+
+
+    for(let i = 1; i <= pageCount; i++){
+
+        pagination.innerHTML += `
+
+        <button onclick="movePage(${i})">
+            ${i}
+        </button>
+
+        `;
+
+    }
+
+}
+
+
+// 페이지 이동
+function movePage(page){
+
+    currentPage = page;
+
+    showProducts();
+
+}
